65138765db262926acc8823089cbbe7c04275753
Christian Scott <christianscott@Christians-MacBook-Air-2.local>
2023-03-13 18:04:57 -0400

    Updated backend config.

--- tf/env/global/foundation/main.tf
+++ tf/env/global/foundation/main.tf
@@ -16,6 +16,8 @@ terraform {
     storage_account_name = "stsandboxeus"
     container_name       = "sandbox"
     key                  = "terraform.tfstate"
+    tenant_id = "567e2175-bf4e-4bcc-b114-335fa0061f2f"
+    subscription_id = "62c223af-3ea4-4cf8-bb4a-c8449fe872e1"
   }
 }
 
