e7b51cbb3be7e59994ac7e33f1eeb4170e1d7e4d
Christian Scott <christianscott@Christians-MacBook-Air-2.local>
2023-03-20 09:27:28 -0400

    Updated authentication for TF in the pipeline.

--- tf/env/global/foundation/main.tf
+++ tf/env/global/foundation/main.tf
@@ -16,6 +16,7 @@ terraform {
     storage_account_name = "stsandboxeus"
     container_name       = "sandbox"
     key                  = "terraform.tfstate"
+    use_oidc = true
     tenant_id            = "567e2175-bf4e-4bcc-b114-335fa0061f2f"
     subscription_id      = "62c223af-3ea4-4cf8-bb4a-c8449fe872e1"
   }
@@ -24,6 +25,7 @@ terraform {
 # Configure the providers
 provider "azurerm" {
   features {}
+  use_oidc = true
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
 }
